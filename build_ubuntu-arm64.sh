ea99b0694ff0722e075213287b503e29f2684dfc
Nils Schubert <nils.schubert@dima.tu-berlin.de>
2024-07-25 09:21:48 +0200

    Increased the number of cores for building

--- build_ubuntu-arm64.sh
+++ build_ubuntu-arm64.sh
@@ -31,13 +31,13 @@ cmake -G Ninja -S llvm -B build -DCMAKE_BUILD_TYPE=Release \
 				-DLLVM_ENABLE_TERMINFO=OFF \
 				-DLLVM_ENABLE_Z3_SOLVER=OFF \
                 -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt" 
-ninja -j4 -C build 
-ninja -j4 -C build clang-format
-ninja -j4 -C build clang-tidy
-ninja -j4 -C build llvm-cov
-ninja -j4 -C build runtimes
-ninja -j4 -C build install
-ninja -j4 -C build install llvm-cov
+ninja -j16 -C build
+ninja -j16 -C build clang-format
+ninja -j16 -C build clang-tidy
+ninja -j16 -C build llvm-cov
+ninja -j16 -C build runtimes
+ninja -j16 -C build install
+ninja -j16 -C build install llvm-cov
 #ninja -C build mlir-libraries mlir-cmake-exports mlir-headers
 
 # remove stuff from build
