9b6524fadc20e856c8cc37b32e44d5590ccb0bc9
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-09-05 12:02:21 +0200

    Build llvm with libc++

--- build_ubuntu-x64.sh
+++ build_ubuntu-x64.sh
@@ -16,8 +16,20 @@
 cd /build_dir/llvm-project
 rm -rf ./build
 mkdir build
+
+if [ "$STDLIB" == "libc++" ]; then
+    CXX_FLAGS="-stdlib=libc++ -std=c++20"
+    LINKER_FLAGS="-lc++"
+elif [ "$STDLIB" == "stdlibc++" ]; then
+    CXX_FLAGS="-std=c++20"
+    LINKER_FLAGS=""
+else
+    echo "Error: STDLIB env not set to either libc++ or stdlibc++."
+    exit 1
+fi
+
 cmake -G Ninja -S llvm -B build -DCMAKE_BUILD_TYPE=Release \
-			    -DLLVM_ENABLE_PROJECTS="clang;lld;mlir;clang-tools-extra"   \
+    			        -DLLVM_ENABLE_PROJECTS="clang;lld;mlir;clang-tools-extra"   \
 				-DBOOTSTRAP_LLVM_ENABLE_LTO=ON \
 				-DLLVM_INCLUDE_EXAMPLES=OFF    \
 				-DLLVM_INCLUDE_TESTS=OFF \
@@ -30,7 +42,9 @@ cmake -G Ninja -S llvm -B build -DCMAKE_BUILD_TYPE=Release \
 				-DLLVM_BUILD_TOOLS=ON \
 				-DLLVM_ENABLE_TERMINFO=OFF \
 				-DLLVM_ENABLE_Z3_SOLVER=OFF \
-                -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt" 
+				-DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+				-DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+				-DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt" 
 ninja -C build 
 ninja -C build clang-format
 ninja -C build clang-tidy
